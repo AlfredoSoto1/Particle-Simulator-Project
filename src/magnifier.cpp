577b2b1e6b0943cdcf680bb5a5137386bf18c753
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2022-09-14 23:38:18 -0400

    fixed bug - magnifing with no box shown

--- src/magnifier.cpp
+++ src/magnifier.cpp
@@ -34,13 +34,15 @@ void Magnifier::reset(const int& button) {
 }
 
 void Magnifier::loadDraggedPosition(const int& x, const int& y) {
-    this->lastPosition.x = x;
-    this->lastPosition.y = y;
+    if(currentButtonClicked == OF_MOUSE_BUTTON_LEFT) {
+        this->lastPosition.x = x;
+        this->lastPosition.y = y;
+    }
 }
 
 void Magnifier::loadInitialPosition(const int& x, const int& y, const int& button) {
     currentButtonClicked = button;
-    if(button == OF_MOUSE_BUTTON_LEFT) {
+    if(currentButtonClicked == OF_MOUSE_BUTTON_LEFT) {
         if(!holdClick) {
             initialPosition.x = x;
             initialPosition.y = y;
@@ -67,10 +69,10 @@ void Magnifier::loadInitialPosition(const int& x, const int& y, const int& butto
 
 void Magnifier::magnifyParticle(Particle* particle) {
 
-    glm::vec3& particlePosition = particle->pos;
+    glm::vec3& pos = particle->pos;
 
-    bool xPossibleBoundries = (particlePosition.x > initialPosition.x && particlePosition.x < lastPosition.x) || (particlePosition.x < initialPosition.x && particlePosition.x > lastPosition.x);
-    bool yPossibleBoundries = (particlePosition.y > initialPosition.y && particlePosition.y < lastPosition.y) || (particlePosition.y < initialPosition.y && particlePosition.y > lastPosition.y);
+    bool xPossibleBoundries = (pos.x > initialPosition.x && pos.x < lastPosition.x) || (pos.x < initialPosition.x && pos.x > lastPosition.x);
+    bool yPossibleBoundries = (pos.y > initialPosition.y && pos.y < lastPosition.y) || (pos.y < initialPosition.y && pos.y > lastPosition.y);
 
     if(xPossibleBoundries && yPossibleBoundries) {
         particle->magnifyingfactor = 3.0;
